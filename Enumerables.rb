eca3dce3628071a524b610ca915961ede7b3a534
Anasscodes <rockbell8@gmail.com>
2021-05-11 01:40:47 +0300

    Add class Array

--- Enumerables.rb
+++ Enumerables.rb
@@ -1,4 +1,4 @@
-# My_each
+module Enumerable
 
 def my_each (array)
   i = 0
@@ -8,4 +8,20 @@ def my_each (array)
   end
 end
 
+
 my_each[1, 2, 3, 4, 5] { |i| puts i ** 2 }
+
+
+class   Array 
+    def my_each
+        i = 0
+        while i < self.length
+            yield self[i]
+            i +=1
+        end
+    end
+end
+
+
+[1, 2, 3, 4, 5].my_each { |i| puts i ** 2 }
+
