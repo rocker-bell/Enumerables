16f977b156b6a84bfcd0cf507f8d1172286de09e
Anasscodes <rockbell8@gmail.com>
2021-05-17 11:12:47 +0300

    Add defintion of methods

--- Enumerables.rb
+++ Enumerables.rb
@@ -1,27 +1,68 @@
 module Enumerable
+#my_each
+  def my_each(array)
+    i = 0
+    while i < array.length
+      yield array[i]
+      i += 1
+    end
+  end
 
-def my_each (array)
+  my_each[1, 2, 3, 4, 5] { |i| puts i ** 2 }
+
+  class Array
+    def my_each
+      i = 0
+      while i < self.length
+        yield self[i]
+        i += 1
+      end
+    end
+  end
+end
+
+[1, 2, 3, 4, 5].my_each { |i| puts i ** 2 }
+
+#my_each_with_index
+
+def my_each_with_index
+  return to_enum(:my_each_with_index) unless block_given?
   i = 0
-  while i < array.length
-    yield array[i]
-    i += 1
+  my_each do |item|
+    yield(item, i)
+    i =+ 1
   end
 end
 
 
-my_each[1, 2, 3, 4, 5] { |i| puts i ** 2 }
+def my_select(Array)
+  return to_enum(:my_select) unless block_given?
+  array = Array.new()
+  my_each do |item|
+    array.push(yield item)
+  end
+  array
+end
 
+def my_map(Array)
+  return to_enum(:my_map) unless block_given?
+  array = Array.new()
+  my_each do |item|
+    array.push(yield item)
+  end
+  array
+end
 
-class   Array 
-    def my_each
-        i = 0
-        while i < self.length
-            yield self[i]
-            i +=1
-        end
-    end
+def my_all(Array)
+  Array.my_each do |item|
+    return true unless block_given?
+    true_false yield(item)
+    return false unless true_false
+  end
+  return true
 end
 
 
-[1, 2, 3, 4, 5].my_each { |i| puts i ** 2 }
+
+
 
